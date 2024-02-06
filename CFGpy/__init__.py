9c02c4baabce9c7ca3a371d5dcb01f7b1ce9c2b1
Roy Gutglick <roygutg@gmail.com>
2024-02-06 18:07:09 +0200

    tidy up init

--- CFGpy/__init__.py
+++ CFGpy/__init__.py
@@ -1,29 +1,3 @@
 from ._version import __version__
-import os
-
-
-def get_nas_path():
-    default_nas_path = r"\\132.64.186.144\HartLabNAS"
-    environment_variable_name = "NAS_PATH"
-
-    if os.path.isdir(default_nas_path):
-        return default_nas_path
-    try:
-        env_var = os.environ[environment_variable_name]  # may raise KeyError
-        if os.path.isdir(env_var):
-            return env_var
-
-        error_msg = f"Path indicated by the NAS_PATH environment variable does not exist: {env_var}"
-
-    except KeyError:  # environment variable doesn't exist
-        error_msg = f"Lab NAS has to be mapped to {default_nas_path} or to a path specified by the NAS_PATH " \
-                    f"environment variable.\nFor details, see https://comdepri.slab.com/posts/connecting-to-nas-ir4z367g"
-
-    raise NotADirectoryError(error_msg)
-
-
-NAS_PATH = get_nas_path()
-
-# the following line has to appear after NAS_PATH is defined, because sub-packages use import CFGpy.NAS_PATH.
-# Importing them before NAS_PATH exists results in circular import
-from . import behavioral, utils
+from ._nas_path import NAS_PATH
+from . import behavioral, utils  # this has to appear after import NAS_PATH to avoid circular import with submodules
