8d7efb7b0a929ddf788c43075ae49433c817f70e
Roy Gutglick <roygutg@gmail.com>
2024-02-16 16:10:09 +0200

    now uses pytest

--- tests/test_behavioral_pipeline.py
+++ tests/test_behavioral_pipeline.py
@@ -1,3 +1,4 @@
+import pytest
 from CFGpy.behavioral import Downloader, Parser, Preprocessor, MeasureCalculator
 from CFGpy import NAS_PATH
 import os
@@ -10,92 +11,79 @@ TEST_PARSED_OLD_FORMAT_FILENAME = "test_parsed_old_format.txt"
 TEST_PREPROCESSED_FILENAME = "test_preprocessed.json"
 TEST_MEASURES_FILENAME = "test_measures.csv"
 
-
-def test_downloader():
-    for filename in os.listdir(TEST_FILES_DIR):
-        absolute_path = os.path.join(TEST_FILES_DIR, filename)
-        if os.path.isdir(absolute_path):
-
-            with open(os.path.join(absolute_path, RED_METRICS_URL_FILENAME)) as url_fp:
-                url = url_fp.read()
-            Downloader(url, "event.csv").download()
-
-            with open(os.path.join(absolute_path, TEST_DOWNLOADED_FILENAME), "r") as test_event_fp:
-                test_event = test_event_fp.read()
-            with open("event.csv", "r") as event_fp:
-                event = event_fp.read()
-
-            assert test_event == event
+test_dirs = []
+for filename in os.listdir(TEST_FILES_DIR):
+    absolute_path = os.path.join(TEST_FILES_DIR, filename)
+    if os.path.isdir(absolute_path):
+        test_dirs.append(absolute_path)
 
 
-def test_parser():
-    for filename in os.listdir(TEST_FILES_DIR):
-        absolute_path = os.path.join(TEST_FILES_DIR, filename)
-        if os.path.isdir(absolute_path):
-            parser = Parser.from_file(os.path.join(absolute_path, TEST_DOWNLOADED_FILENAME))
-            parser.parse()
-            parser.dump("parsed.json")
+@pytest.mark.parametrize("test_dir", test_dirs)
+def test_downloader(test_dir):
+    with open(os.path.join(test_dir, RED_METRICS_URL_FILENAME)) as url_fp:
+        url = url_fp.read()
+    Downloader(url, "event.csv").download()
 
-            with open(os.path.join(absolute_path, TEST_PARSED_FILENAME), "r") as test_parsed_fp:
-                test_parsed = test_parsed_fp.read()
-            with open("parsed.json", "r") as parsed_fp:
-                parsed = parsed_fp.read()
+    with open(os.path.join(test_dir, TEST_DOWNLOADED_FILENAME), "r") as test_event_fp:
+        test_event = test_event_fp.read()
+    with open("event.csv", "r") as event_fp:
+        event = event_fp.read()
 
-            assert test_parsed == parsed
+    assert test_event == event
 
 
-def test_parser_conversion_to_old_format():
-    for filename in os.listdir(TEST_FILES_DIR):
-        absolute_path = os.path.join(TEST_FILES_DIR, filename)
-        if filename == "set2" and os.path.isdir(absolute_path):
-            parser = Parser.from_file(os.path.join(absolute_path, TEST_DOWNLOADED_FILENAME))
-            parsed_new_format = parser.parse()
-            parsed_old_format = Parser.translate_parsed_results_to_mathematica(parsed_new_format)
+@pytest.mark.parametrize("test_dir", test_dirs)
+def test_parser(test_dir):
+    parser = Parser.from_file(os.path.join(test_dir, TEST_DOWNLOADED_FILENAME))
+    parser.parse()
+    parser.dump("parsed.json")
 
-            with open(r"parsed_old_fmt.txt", "w") as parsed_old_format_fp:
-                parsed_old_format_fp.write(parsed_old_format)
+    with open(os.path.join(test_dir, TEST_PARSED_FILENAME), "r") as test_parsed_fp:
+        test_parsed = test_parsed_fp.read()
+    with open("parsed.json", "r") as parsed_fp:
+        parsed = parsed_fp.read()
 
-            with open(os.path.join(absolute_path, TEST_PARSED_OLD_FORMAT_FILENAME), "r") as test_parsed_old_format_fp:
-                test_parsed_old_format = test_parsed_old_format_fp.read()
-
-            assert test_parsed_old_format == parsed_old_format
+    assert test_parsed == parsed
 
 
-def test_processor():
-    for filename in os.listdir(TEST_FILES_DIR):
-        absolute_path = os.path.join(TEST_FILES_DIR, filename)
-        if os.path.isdir(absolute_path):
-            preprocessor = Preprocessor.from_json(os.path.join(absolute_path, TEST_PARSED_FILENAME))
-            preprocessor.preprocess()
-            preprocessor.dump("preprocessed.json")
+@pytest.mark.parametrize("test_dir", test_dirs)
+def test_parser_conversion_to_old_format(test_dir):
+    parser = Parser.from_file(os.path.join(test_dir, TEST_DOWNLOADED_FILENAME))
+    parsed_new_format = parser.parse()
+    parsed_old_format = Parser.translate_parsed_results_to_mathematica(parsed_new_format)
 
-            with open(os.path.join(absolute_path, TEST_PREPROCESSED_FILENAME), "r") as test_preprocessed_fp:
-                test_preprocessed = test_preprocessed_fp.read()
-            with open("preprocessed.json", "r") as preprocessed_fp:
-                preprocessed = preprocessed_fp.read()
+    with open(r"parsed_old_fmt.txt", "w") as parsed_old_format_fp:
+        parsed_old_format_fp.write(parsed_old_format)
 
-            assert test_preprocessed == preprocessed
+    with open(os.path.join(test_dir, TEST_PARSED_OLD_FORMAT_FILENAME), "r") as test_parsed_old_format_fp:
+        test_parsed_old_format = test_parsed_old_format_fp.read()
 
-
-def test_measures_calculator():
-    for filename in os.listdir(TEST_FILES_DIR):
-        absolute_path = os.path.join(TEST_FILES_DIR, filename)
-        if os.path.isdir(absolute_path):
-            measures_calculator = MeasureCalculator.from_json(os.path.join(absolute_path, TEST_PARSED_FILENAME))
-            measures_calculator.calc()
-            measures_calculator.dump("measures.csv")
-
-            with open(os.path.join(absolute_path, TEST_MEASURES_FILENAME), "r") as test_measures_fp:
-                test_measures = test_measures_fp.read()
-            with open("measures.csv", "r") as measures_fp:
-                measures = measures_fp.read()
-
-            assert test_measures == measures
+    assert test_parsed_old_format == parsed_old_format
 
 
-if __name__ == '__main__':
-    # test_downloader()
-    # test_parser()
-    test_parser_conversion_to_old_format()
-    # test_processor()
-    # test_measures_calculator()
+@pytest.mark.parametrize("test_dir", test_dirs)
+def test_processor(test_dir):
+    preprocessor = Preprocessor.from_json(os.path.join(test_dir, TEST_PARSED_FILENAME))
+    preprocessor.preprocess()
+    preprocessor.dump("preprocessed.json")
+
+    with open(os.path.join(test_dir, TEST_PREPROCESSED_FILENAME), "r") as test_preprocessed_fp:
+        test_preprocessed = test_preprocessed_fp.read()
+    with open("preprocessed.json", "r") as preprocessed_fp:
+        preprocessed = preprocessed_fp.read()
+
+    assert test_preprocessed == preprocessed
+
+
+@pytest.mark.parametrize("test_dir", test_dirs)
+def test_measures_calculator(test_dir):
+    measures_calculator = MeasureCalculator.from_json(os.path.join(test_dir, TEST_PARSED_FILENAME))
+    measures_calculator.calc()
+    measures_calculator.dump("measures.csv")
+
+    with open(os.path.join(test_dir, TEST_MEASURES_FILENAME), "r") as test_measures_fp:
+        test_measures = test_measures_fp.read()
+    with open("measures.csv", "r") as measures_fp:
+        measures = measures_fp.read()
+
+    assert test_measures == measures
