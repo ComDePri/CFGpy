ec0ef46505c1fc4984e370238291426b078b20b0
Roy Gutglick <roygutg@gmail.com>
2024-05-27 15:53:07 +0300

    segment_explore_exploit handles 0-gallery games

--- CFGpy/behavioral/_utils.py
+++ CFGpy/behavioral/_utils.py
@@ -62,10 +62,15 @@ def coords_to_bin_coords(coords):
 
 
 def segment_explore_exploit(shapes, min_save_for_exploit=MIN_SAVE_FOR_EXPLOIT):
+    n_shapes = len(shapes)
+    no_exploit_return_value = [(0, n_shapes)], []
     shapes_df = pd.DataFrame(shapes)
 
     gallery_saves = shapes_df[SHAPE_SAVE_TIME_IDX]
     gallery_indices = np.flatnonzero(gallery_saves.notna())
+    if not any(gallery_indices):
+        return no_exploit_return_value
+
     gallery_in_times = shapes_df.iloc[gallery_indices][SHAPE_MOVE_TIME_IDX]
     gallery_out_times = shapes_df.iloc[gallery_indices][SHAPE_MAX_MOVE_TIME_IDX]
     gallery_diffs = gallery_in_times - gallery_out_times.shift()
@@ -95,7 +100,7 @@ def segment_explore_exploit(shapes, min_save_for_exploit=MIN_SAVE_FOR_EXPLOIT):
 
     n_shapes = len(shapes)
     if not exploit_slices:
-        return [(0, n_shapes)], []
+        return no_exploit_return_value
 
     exploit_end = exploit_slices[-1][1]
     explore_end = explore_slices[-1][1]
