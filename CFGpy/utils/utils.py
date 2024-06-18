90aca092f7eb7478a1338e182bd6fbb6e8faac11
roygutg <roy.gutglick@mail.huji.ac.il>
2024-06-18 16:23:27 +0300

    determine if a shortest path is already calculated using `if` instead of abusing exception catching

--- CFGpy/utils/utils.py
+++ CFGpy/utils/utils.py
@@ -100,15 +100,14 @@ def get_shortest_path_len(shape1, shape2):
     with open(SHORTEST_PATHS_DICT_PATH) as shortest_path_len_fp:
         shortest_path_len_dict = json.load(shortest_path_len_fp)
 
-    try:
-        shortest_path_len = shortest_path_len_dict[key]
-    except KeyError:
-        shape_network = nx.read_adjlist(os.path.join(CFG_RESOURCES_PATH, "all_shapes.adjlist"), nodetype=int)
-        shortest_path_len = nx.shortest_path_length(shape_network, source=shape1, target=shape2)
-        shortest_path_len_dict[key] = shortest_path_len
-        with open(SHORTEST_PATHS_DICT_PATH, "w") as shortest_path_len_fp:
-            json.dump(shortest_path_len_dict, shortest_path_len_fp)
+    if key in shortest_path_len_dict:
+        return shortest_path_len_dict[key]
 
+    shape_network = nx.read_adjlist(os.path.join(CFG_RESOURCES_PATH, "all_shapes.adjlist"), nodetype=int)
+    shortest_path_len = nx.shortest_path_length(shape_network, source=shape1, target=shape2)
+    shortest_path_len_dict[key] = shortest_path_len
+    with open(SHORTEST_PATHS_DICT_PATH, "w") as shortest_path_len_fp:
+        json.dump(shortest_path_len_dict, shortest_path_len_fp)
     return shortest_path_len
 
 
