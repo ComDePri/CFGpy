6cf18620bc13fc05b1933d0f2d9d09f9c8240ea1
roygutg <roy.gutglick@mail.huji.ac.il>
2024-09-03 03:14:51 +0300

    separated imports, to get more informative tracebacks if there's an error

--- CFGpy/__init__.py
+++ CFGpy/__init__.py
@@ -1,3 +1,4 @@
 from ._version import __version__
 from ._nas_path import NAS_PATH
-from . import behavioral, utils  # this has to appear after import NAS_PATH to avoid circular import with submodules
+from . import utils  # this has to appear after import NAS_PATH to avoid circular import with submodules
+from . import behavioral
