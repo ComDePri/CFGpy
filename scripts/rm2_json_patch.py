082598759dbf282a6d9e2145bcd17cee5321e0b4
roygutg <roy.gutglick@mail.huji.ac.il>
2024-12-01 15:18:57 +0200

    adapted to current version of dev

--- scripts/rm2_json_patch.py
+++ scripts/rm2_json_patch.py
@@ -1,14 +1,14 @@
 import json
-from CFGpy.behavioral import Downloader, Parser, PostParser, FeatureExtractor, config
+from CFGpy.behavioral import Downloader, Parser, PostParser, FeatureExtractor, Configuration
 
 
-class RM2_JSON2CSV(Downloader):
+class RM2Converter(Downloader):
     def __init__(self, rm2_json_filename, *downloader_args, **downloader_kwargs):
         super().__init__(*downloader_args, **downloader_kwargs)
         self.rm2_json_filename = rm2_json_filename
         self.rm1_format = None
 
-    def convert(self):
+    def convert_rm2_to_rm1_json(self):
         with open(self.rm2_json_filename) as f:
             rm2 = json.load(f)
 
@@ -30,46 +30,51 @@ class RM2_JSON2CSV(Downloader):
 
         self.rm1_format = rm1
 
-    def create_output(self, verbose):
+    def create_output(self, input_json, verbose=False):
         output_json = []
 
-        for event in self.rm1_format:
+        for event in input_json:
             # Filter out common fields
-            output_json_record = {k: v for (k, v) in event.items() if k in self.common_fields}
+            output_json_record = {k: v for (k, v) in event.items() if k in self.config.DOWNLOADER_COMMON_FIELDS}
 
             # Handle custom data
-            if config.EVENT_CUSTOM_DATA_KEY in event:
-                if isinstance(event[config.EVENT_CUSTOM_DATA_KEY], dict):
+            if self.config.EVENT_CUSTOM_DATA_KEY in event:
+                if isinstance(event[self.config.EVENT_CUSTOM_DATA_KEY], dict):
                     # Add each key as a custom data field
-                    for key, value in event[config.EVENT_CUSTOM_DATA_KEY].items():
-                        keyName = f"{config.EVENT_CUSTOM_DATA_KEY}.{key}"
+                    for key, value in event[self.config.EVENT_CUSTOM_DATA_KEY].items():
+                        keyName = f"{self.config.EVENT_CUSTOM_DATA_KEY}.{key}"
                         self.custom_data_fields.add(keyName)
                         output_json_record[keyName] = value
                 else:
-                    self.custom_data_fields.add(config.EVENT_CUSTOM_DATA_KEY)
+                    self.custom_data_fields.add(self.config.EVENT_CUSTOM_DATA_KEY)
 
-            player_id = event[config.EVENT_PLAYER_ID_KEY]
-            output_json_record[config.RAW_PLAYER_ID] = player_id
-            output_json_record[config.RAW_PLAYER_EXTERNAL_ID] = event["playerExternalId"]
-            output_json_record[config.RAW_PLAYER_CUSTOM_DATA] = event["playerCustomData"]
+            player_id = event[self.config.EVENT_PLAYER_ID_KEY]
+            output_json_record[self.config.RAW_PLAYER_ID] = player_id
+            output_json_record[self.config.RAW_PLAYER_EXTERNAL_ID] = event["playerExternalId"]
+            output_json_record[self.config.RAW_PLAYER_CUSTOM_DATA] = event["playerCustomData"]
             output_json.append(output_json_record)
 
         return output_json
 
-    def process_json(self, verbose=True):
-        self.convert()
-        output_json = self.create_output(verbose)
+    def convert_rm2_json_to_csv(self, verbose=True):
+        self.convert_rm2_to_rm1_json()
+        output_json = self.create_output(self.rm1_format, verbose)
         self.write_csv(output_json, verbose)
 
 
+rm2_config = Configuration.default()
+rm2_config.SERVER_DATE_FORMAT = '%Y-%m-%dT%H:%M:%S%z'
+
+# start from RM2 json
 rm2_json_filename = r"/home/royg/home/royg/CFGpy/CFGpy/behavioral/rm2.json"
 raw_data_filename = "event.csv"
-rm2_json2csv = RM2_JSON2CSV(rm2_json_filename=rm2_json_filename, output_filename=raw_data_filename)
-rm2_json2csv.process_json()
+rm2_converter = RM2Converter(rm2_json_filename=rm2_json_filename, output_filename=raw_data_filename, config=rm2_config)
+rm2_converter.convert_rm2_json_to_csv()
 
-parsed_data = Parser.from_file(raw_data_filename)
-postparsed_data = PostParser(parsed_data).postparse()
-feature_extractor = FeatureExtractor(postparsed_data)
+# continue pipeline
+parsed_data = Parser.from_file(raw_data_filename, config=rm2_config).parse()
+postparsed_data = PostParser(parsed_data, config=rm2_config).postparse()
+feature_extractor = FeatureExtractor(postparsed_data, config=rm2_config)
 feature_extractor.extract(verbose=True)
 
 features_filename = "features.csv"
