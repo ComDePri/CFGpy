f3ede23aac1530d59ee8550b07924d150338da17
Roni Eisenberg <ronimkatz@gmail.com>
2025-02-04 11:31:33 +0200

    change tests to pytest

--- tests/test_file_access.py
+++ tests/test_file_access.py
@@ -1,43 +1,43 @@
+import pytest
 import os
 from pathlib import Path
-import unittest
 from CFGpy.utils import FilesHandler, FileNames
 
 
-class FilesHandlerTests(unittest.TestCase):
-    
-    def setUp(self):
-        self.files_handler: FilesHandler = FilesHandler()
-    
-    def test_clear_cache(self):
-        self.files_handler.clear_cache()
-        for f in FileNames.ALL_FILES:
-            self.assertFalse(os.path.exists(os.path.join(FileNames.CACHE_DIR, f)))
-            
-    def test_get_files_from_github(self):
-        self.files_handler.clear_cache()
-        for f in FileNames.ALL_FILES:
-            self.assertFalse(os.path.exists(os.path.join(FileNames.CACHE_DIR, f)))
-            self.files_handler.get_raw_file_from_github(file_name=f, branch=os.getenv("GIT_BRANCH", "main"))
-            self.assertTrue(os.path.exists(os.path.join(FileNames.CACHE_DIR, f)))
-    
-    def test_load_json_data(self):
-        json_data: dict = self.files_handler.load_json_data(file_name="test_json_file.json", dir_path=os.path.join(Path(__file__).parent, "test_files"))
-        self.assertEqual(type(json_data), dict)
-        self.assertEqual(len(json_data), 50)
-        self.assertEqual(set(json_data.values()), {0, 1, 2, 3, 4})
-        self.assertEqual(len(json_data.keys()), 50)
-    
-    def test_files_handler_properties(self):
-        self.files_handler.clear_cache()
-        self.assertGreater(len(self.files_handler.vanilla_data), 1)
-        self.assertGreater(len(self.files_handler.vanilla_features), 1)
-        self.assertGreater(len(self.files_handler.vanilla_gallery_counter), 1)
-        self.assertGreater(len(self.files_handler.vanilla_giant_component), 1)
-        self.assertGreater(len(self.files_handler.vanilla_step_counter), 1)
-        self.assertGreater(len(self.files_handler.shape_network), 1)
-        self.assertGreater(len(self.files_handler.id2coord), 1)
-        self.assertGreater(len(self.files_handler.shortest_paths_dict), 1)
+@pytest.fixture
+def files_handler():
+    return FilesHandler()
 
-if __name__ == "__main__":
-    unittest.main()
+
+def test_clear_cache(files_handler):
+    files_handler.clear_cache()
+    for f in FileNames.ALL_FILES:
+        assert not os.path.exists(os.path.join(FileNames.CACHE_DIR, f))
+
+
+def test_get_files_from_github(files_handler):
+    files_handler.clear_cache()
+    for f in FileNames.ALL_FILES:
+        assert not os.path.exists(os.path.join(FileNames.CACHE_DIR, f))
+        files_handler.get_raw_file_from_github(file_name=f, branch=os.getenv("GIT_BRANCH", "main"))
+        assert os.path.exists(os.path.join(FileNames.CACHE_DIR, f))
+
+
+def test_load_json_data(files_handler):
+    json_data = files_handler.load_json_data(file_name="test_json_file.json", dir_path=os.path.join(Path(__file__).parent, "test_files"))
+    assert isinstance(json_data, dict)
+    assert len(json_data) == 50
+    assert set(json_data.values()) == {0, 1, 2, 3, 4}
+    assert len(json_data.keys()) == 50
+
+
+def test_files_handler_properties(files_handler):
+    files_handler.clear_cache()
+    assert len(files_handler.vanilla_data) > 1
+    assert len(files_handler.vanilla_features) > 1
+    assert len(files_handler.vanilla_gallery_counter) > 1
+    assert len(files_handler.vanilla_giant_component) > 1
+    assert len(files_handler.vanilla_step_counter) > 1
+    assert len(files_handler.shape_network) > 1
+    assert len(files_handler.id2coord) > 1
+    assert len(files_handler.shortest_paths_dict) > 1
