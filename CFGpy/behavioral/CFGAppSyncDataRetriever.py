8b46f0b3ffaa9a1689bbf513204f39edf7bf2719
Dan Amir <dan.amir123@gmail.com>
2026-03-25 18:13:47 +0200

    ensuring that playerCustomData is json parsable in the new downloader

--- CFGpy/behavioral/CFGAppSyncDataRetriever.py
+++ CFGpy/behavioral/CFGAppSyncDataRetriever.py
@@ -495,7 +495,7 @@ class CFGAppSyncDataRetriever(DataRetriever):
                 self._config.EVENT_CUSTOM_DATA_KEY: ev.get("data") if ev.get("data") is not None else "",
             }
             if row["sessionMetadata"]:
-                row["playerCustomData"] = row["sessionMetadata"].get("customData") if (row["sessionMetadata"].get("customData") is not None) else {}
+                row["playerCustomData"] = json.dumps(row["sessionMetadata"].get("customData")) if (row["sessionMetadata"].get("customData") is not None) else "{}"
             player_metadata = row.get("playerMetadata", {})
             row[self._config.RAW_PLAYER_BIRTHDATE] = player.get("birthDate", None)
             row[self._config.RAW_PLAYER_REGION] = player.get("region", None)
